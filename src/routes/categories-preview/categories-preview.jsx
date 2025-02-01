d6c645ceffc909c945c7270beada8ae51141315c
mariia.tkachova <mashovozka@gmail.com>
2025-02-01 18:30:03 +0100

    added firebase db

---
+++ src/routes/categories-preview/categories-preview.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { CategoriesContext } from "../../context/categories.context";
+import CategoryPreview from "../../components/category-preview/category-preview";
+
+const CategoriesPreview = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
+
+  return (
+    <>
+      {Object.keys(categoriesMap).map((key) => {
+        const products = categoriesMap[key];
+        return <CategoryPreview key={key} title={key} products={products} />;
+      })}
+    </>
+  );
+};
+
+export default CategoriesPreview;
